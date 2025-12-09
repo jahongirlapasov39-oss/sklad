1af362c713eac0a84b8ffb632d1f8696bfbe861e
uzlitefas <uzlitefas00@gmail.com>
2025-12-09 18:26:38 +0500

    routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/shared/dashboard";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={"login"} />
+        <Route
+          path="/admin"
+          element={
+            <div className="flex w-full">
+              <Dashboard />
+              <div>
+                header <Outlet />
+              </div>
+            </div>
+          }
+        >
+          <Route path="" element={"home"} />
+          <Route path="aylanma" element={"Kirim & Chiqim"} />
+          <Route path="manager" element={"managerlar"} />
+          <Route path="deps" element={"qarzlar"} />
+        </Route>
+        <Route
+          path="/manager"
+          element={
+            <div>
+              dashboard{" "}
+              <div>
+                header <Outlet />
+              </div>
+            </div>
+          }
+        >
+          <Route path="" element={"Sotish"} />
+          <Route path="aylanma" element={"Kirim & Chiqim"} />
+          <Route path="deps" element={"qarzlar"} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/shared/dashboard.tsx
@@ -0,0 +1,14 @@
+function Dashboard() {
+  return (
+    <div className="bg-gray-100/50 h-screen w-60">
+      <div className="p-5 grid gap-3 font-medium">
+        <h1>Home</h1>
+        <h1>qarzlar</h1>
+        <h1>kirim chiqim</h1>
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
